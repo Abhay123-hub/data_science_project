d75b2defe0670675954b70430af0759ed1ab5e15
Abhay3002 <rajputjiabhay3002@gmail.com>
2025-01-08 12:19:52 +0530

    added1

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 from src.datascience import logger
+from src.datascience.pipeline.data_ingestion import DataIngestionTrainingPipeLine 
 
 logger.info("Welcome to our custom logging data science ")
+STAGE_NAME = "DATA INGESTION"
+
+
+## now doing execution of my code
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>> {STAGE_NAME} started <<<<<")
+        obj = DataIngestionTrainingPipeLine()
+        obj.initiate_data_ingestion()
+        logger.info(f">>> Stage {STAGE_NAME} completed")
+    except Exception as e:
+        raise e
 
 
